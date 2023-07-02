7867757d3417f9177fc24e976ce176d375680fa2
heli <helipatel56789@gmail.com>
2023-07-01 23:16:22 -0400

    final design

---
+++ app/src/main/java/com/hk/project/mannual_timeEntry.java
@@ -0,0 +1,14 @@
+package com.hk.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mannual_timeEntry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mannual_time_entry);
+    }
+}
